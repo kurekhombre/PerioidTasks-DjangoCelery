66dde9e5b63813ede8bc908c7b657b3208b8cb6d
Karol Kurek <57529221+kurekhombre@users.noreply.github.com>
2022-10-19 13:11:47 +0200

    Update settings.py

--- periodictasks/settings.py
+++ periodictasks/settings.py
@@ -143,8 +143,8 @@ CELERY_BEAT_SCHEDULER = 'django_celery_beat.schedulers:DatabaseScheduler'
 # SMTP SETTINGS
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'kurekhombre@gmail.com'
-EMAIL_HOST_PASSWORD = 'qvprarkbbrdbzezd'
+EMAIL_HOST_USER = ###
+EMAIL_HOST_PASSWORD = ###
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 
